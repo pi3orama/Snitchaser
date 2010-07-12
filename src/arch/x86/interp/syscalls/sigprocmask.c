c87876e18f6afcc12a7b568a389100f287741046
Wang Nan <pi3orama@gmail.com>
2010-07-12 10:52:27 +0800

    save signal stack

---
+++ src/arch/x86/interp/syscalls/sigprocmask.c
@@ -0,0 +1,16 @@
+/* 
+ * sigprocmask.c
+ * by WN @ Jun. 01, 2010
+ */
+
+#include "syscall_handler.h"
+#include <common/debug.h>
+#include <xasm/syscall.h>
+
+#ifdef PRE_LIBRARY
+
+
+#endif
+
+// vim:ts=4:sw=4
+
