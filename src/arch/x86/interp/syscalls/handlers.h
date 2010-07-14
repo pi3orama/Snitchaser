39495b1305affd26462e72876ba64ee04b752e00
Wang Nan <pi3orama@gmail.com>
2010-07-15 00:54:21 +0800

    ignored signals OK.

--- src/arch/x86/interp/syscalls/handlers.h
+++ src/arch/x86/interp/syscalls/handlers.h
@@ -13,6 +13,7 @@ def_complex_handler(exit)
 def_handler(rt_sigprocmask)
 def_handler(sigprocmask)
 def_trival_handler(getpid)
+def_trival_handler(kill)
 //def_complex_handler(rt_sigaction)
 
 // vim:ts=4:sw=4
